03e8f8778bca0a6aeb3e8e2e25c596fa5edfa132
hachemchrayti <hachemfst@gmail.Com>
2021-03-17 19:12:47 +0100

    Add and Delete items with ajax (static data)

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,17 +1,35 @@
+var body_parser= require('body-parser')
+
+var  urlencodedParser=body_parser.urlencoded({extended:false})
+var data=[
+{
+    item:'get milk'
+},
+{
+    item:'walking dog'
+},
+{
+    item:'footing'
+}
+]
+
 module.exports=function(app){
 
     app.get('/todo',(req,res)=>{
-        res.render('todo')
+        res.render('todo',{todos:data})
 
     })
 
-    app.post('/todo',(req,res)=>{
-        
+    app.post('/todo',urlencodedParser,(req,res)=>{
+        data.push(req.body)
+        res.json(data)  
 
     })
 
-    app.delete('/todo',(req,res)=>{
-        
-
+    app.delete('/todo/:item',(req,res)=>{
+        data=data.filter(function(todo){
+            return todo.item.replace(/ /g,'-') !== req.params.item;
+        }) ;       
+        res.json(data);
     })
 };
