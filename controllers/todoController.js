ec5f0b641e665bf38c5d640986c6cb03667c06da
hachemchrayti <hachemfst@gmail.Com>
2021-03-17 18:25:48 +0100

    Add MVC structure and index view

---
+++ controllers/todoController.js
@@ -0,0 +1,17 @@
+module.exports=function(app){
+
+    app.get('/todo',(req,res)=>{
+        res.render('todo')
+
+    })
+
+    app.post('/todo',(req,res)=>{
+        
+
+    })
+
+    app.delete('/todo',(req,res)=>{
+        
+
+    })
+};
